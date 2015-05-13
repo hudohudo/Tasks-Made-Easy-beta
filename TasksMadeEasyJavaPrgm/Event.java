19d6aa8f127e0b4b63ececc6f626d3a42c53a294
hudohudo <havenhillhudson@gmail.com>
2015-05-13 09:33:24 -0500

    added to Event

--- TasksMadeEasyJavaPrgm/Event.java
+++ TasksMadeEasyJavaPrgm/Event.java
@@ -1,25 +1,8 @@
-public class TasksMadeEasy {
-	
-	//This may not stay public, idk yet
-	public List<Task> myTasks;
+public class Event extends Task{
 
-	public TasksMadeEasy(){
-			myTasks = new ArrayList<Task>();
+	private Date dateToNotify;
+
+	public Event(){
+		
 	}
-
-	public ArrayList<Task> getTaskLists(){
-		return myTasks;
-	}
-
-	public ArrayList<Task> sortByPriority(){
-		ArrayList<Task> temp = myTasks;
-		for(int ind = 1;ind<temp.size();ind++){
-			if(temp.get(ind - 1).getPriority() > temp.get(ind).getPriority()){
-				Task tempTask = temp.get(ind);
-				temp.get(ind) = temp.get(ind - 1);
-				temp.get(ind - 1) = tempTask;
-			}
-		}
-	}
-
 }
