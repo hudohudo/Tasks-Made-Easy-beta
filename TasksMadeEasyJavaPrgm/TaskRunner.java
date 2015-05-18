1af07b5cb02210cb1d67f2da52c2d8874b06a94f
eqian <eqian7@students.d125.org>
2015-05-18 10:10:52 -0500

    taskrunner

--- TasksMadeEasyJavaPrgm/TaskRunner.java
+++ TasksMadeEasyJavaPrgm/TaskRunner.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 public class TaskRunner{
 	public static void main(String[] args)
 	{
-		Scanner scan = new Scanner(System.in);
-	    System.out.println("Enter in the name of the task");
-	    String name = scan.nextLine();
-	    System.out.println("Enter in the month of the task (1-12 only)");
-		int month = scan.nextInt();
-		System.out.println("Enter in the day of the task (Must be a valid day for the given month)");
-		int day = scan.nextInt();
-		if(((month==1||month==3||month==5||month==6||month==7||month==8||month==10||month==12)&&day>31)||((month==4||month==6||month==9||month==11)&&day>30)||(month==2&&day>28&&year%4!=0)||(month==2&&day>29&&year%4==0))
-		{
-			System.out.println("Not a valid date");
-		}
-		else
-		{
-			System.out.println("Enter in the year of the task")
-			Date theDate = new Date (year, month, day);
-		}
+		boolean running = true;
 		TasksMadeEasy mainTasks = new TasksMadeEasy();
+		Scanner scan = new Scanner(System.in);
+		while(running)
+		{
+			System.out.println("Enter in the name of the task (DONE to exit)");
+	  		String name = scan.nextLine();
+	  		if(name==done) running=false;
+	  		while(running)
+	  		{
+	  	  		System.out.println("Enter in the month of the task (1-12 only)");
+				int month = scan.nextInt();
+				System.out.println("Enter in the day of the task (Must be a valid day for the given month)");
+				int day = scan.nextInt();
+				if(((month==1||month==3||month==5||month==6||month==7||month==8||month==10||month==12)&&day>31)||((month==4||month==6||month==9||month==11)&&day>30)||(month==2&&day>28&&year%4!=0)||(month==2&&day>29&&year%4==0))
+				{
+					System.out.println("Not a valid date");
+				}
+				else
+				{
+					System.out.println("Enter in the year of the task")
+					Date theDate = new Date (year, month, day);
+					mainTasks.addNewTask(new Task(name, theDate));
+				}
+			}
+		}
 		
-		
-		
-		mainTasks.addNewTask(new Task(name, theDate));
-		mainTasks.myTasks.get(0).setPriority(3);
+		mainTasks.myTasks.sortByPriority;
 		mainTasks.sortByPriority();
 		System.out.println("Success!");
 	}
--- TasksMadeEasyJavaPrgm/TasksMadeEasy.java
+++ TasksMadeEasyJavaPrgm/TasksMadeEasy.java
@@ -1,32 +1,33 @@
 import java.util.List;
 import java.util.ArrayList;
-public class TasksMadeEasy {
+public class TasksMadeEasy 
+{
 	
 	//This will not stay public, just need it for testing purposes
 	public ArrayList<Task> myTasks;
 
-	public TasksMadeEasy(){
+	public TasksMadeEasy()
+	{
 			myTasks = new ArrayList<Task>();
 	}
 
-	public ArrayList<Task> getTaskLists(){
+	public ArrayList<Task> getTaskLists()
+	{
 		return myTasks;
 	}
 
-	public ArrayList<Task> sortByPriority(){
+	public void sortByPriority()
+	{
 	    if(myTasks.size() == 0) return myTasks;
-		for(int ind = 1;ind<myTasks.size();ind++){
-			if(myTasks.get(ind - 1).getPriority() > myTasks.get(ind).getPriority()){
-				Task tempTask = myTasks.get(ind);
-				myTasks.set(ind, myTasks.get(ind - 1));
-				myTasks.set(ind - 1, tempTask);
-			}
+		{
+			Collections.sort(myTasks);
 		}
 		return myTasks;
 	}
 	
-	public void addNewTask(Task t){
+	public void addNewTask(Task t)
+	{
 	    myTasks.add(t);
-	   }
+	}
 
 }
