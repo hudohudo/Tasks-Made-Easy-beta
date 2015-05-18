9a764fef73989db62fd7f5314897e77d09926702
hudohudo <havenhillhudson@gmail.com>
2015-05-18 09:07:54 -0500

    new rings fur bad

--- TasksMadeEasyJavaPrgm/Resources.java
+++ TasksMadeEasyJavaPrgm/Resources.java
@@ -4,20 +4,4 @@ public class Resources{
 	private final String ADDNEWTASKMESSAGE = "Please type in the Name of the Task";
 	private final String QUITMESSAGE = "Goodbye, storing Tasks";
 	private final String SUCCESSMESSAGE = "Task created!";
-
-	public String getGreetingMessage(){
-		return GREETINGMESSAGE;
-	}
-
-	public String getAddNewTaskMessage(){
-		return ADDNEWTASKMESSAGE;
-	}
-
-	public String getQuitMessage(){
-		return QUITMESSAGE;
-	}
-
-	public String getSuccessMessage(){
-		return SUCCESSMESSAGE;
-	}
 }
