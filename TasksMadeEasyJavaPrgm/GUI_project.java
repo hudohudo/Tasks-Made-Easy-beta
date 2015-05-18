ec3f720d83d1696f90852b69198b1e02089b9c91
hudohudo <havenhillhudson@gmail.com>
2015-05-18 09:26:13 -0500

    added GUI

---
+++ TasksMadeEasyJavaPrgm/GUI_project.java
@@ -0,0 +1,103 @@
+/**
+ *Text genereted by Simple GUI Extension for BlueJ
+ */
+import javax.swing.UIManager.LookAndFeelInfo;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.border.Border;
+import javax.swing.*;
+
+public class GUI_project extends JFrame {
+    private JMenuBar menuBar;
+    private JPanel panel1;
+    private JTextArea textarea1;
+
+    //Constructor 
+    public GUI_project(){
+
+        this.setTitle("GUI_project");
+        this.setSize(500,400);
+        //menu generate method
+        generateMenu();
+        this.setJMenuBar(menuBar);
+
+        //pane with null layout
+        JPanel contentPane = new JPanel(null);
+        contentPane.setPreferredSize(new Dimension(500,400));
+        contentPane.setBackground(new Color(192,192,192));
+
+        panel1 = new JPanel(null);
+        panel1.setBorder(BorderFactory.createEtchedBorder(1));
+        panel1.setBounds(192,78,150,100);
+        panel1.setBackground(new Color(214,217,223));
+        panel1.setForeground(new Color(0,0,0));
+        panel1.setEnabled(true);
+        panel1.setFont(new Font("sansserif",0,12));
+        panel1.setVisible(true);
+
+        textarea1 = new JTextArea();
+        textarea1.setBounds(-5,-5,150,100);
+        textarea1.setBackground(new Color(255,255,255));
+        textarea1.setForeground(new Color(0,0,0));
+        textarea1.setEnabled(true);
+        textarea1.setFont(new Font("sansserif",0,12));
+        textarea1.setText("This is a test");
+        textarea1.setBorder(BorderFactory.createBevelBorder(1));
+        textarea1.setVisible(true);
+
+        //adding components to contentPane panel
+        contentPane.add(panel1);
+        panel1.add(textarea1);
+
+        //adding panel to JFrame and seting of window position and close operation
+        this.add(contentPane);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    //method for generate menu
+    public void generateMenu(){
+        menuBar = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        JMenu tools = new JMenu("Tools");
+        JMenu help = new JMenu("Help");
+
+        JMenuItem open = new JMenuItem("Open   ");
+        JMenuItem save = new JMenuItem("Save   ");
+        JMenuItem exit = new JMenuItem("Exit   ");
+        JMenuItem preferences = new JMenuItem("Preferences   ");
+        JMenuItem about = new JMenuItem("About   ");
+
+        file.add(open);
+        file.add(save);
+        file.addSeparator();
+        file.add(exit);
+        tools.add(preferences);
+        help.add(about);
+
+        menuBar.add(file);
+        menuBar.add(tools);
+        menuBar.add(help);
+    }
+
+
+    public static void main(String[] args){
+        System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    new GUI_project();
+                }
+            });
+    }
+
+}
