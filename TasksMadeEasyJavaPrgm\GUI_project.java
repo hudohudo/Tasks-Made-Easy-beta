a0c7d212475224900587a4f034c5658618f4be0d
hudohudo <havenhillhudson@gmail.com>
2015-05-18 10:10:39 -0500

    added gui stuff

--- TasksMadeEasyJavaPrgm\GUI_project.java
+++ TasksMadeEasyJavaPrgm\GUI_project.java
@@ -17,61 +17,60 @@ import javax.swing.*;
 
 public class GUI_project extends JFrame {
 
+	private JMenuBar menuBar;
 	private JButton button1;
 	private JComboBox combobox1;
 	private JLabel label1;
 	private JPanel panel2;
-	
-	//50,109,255 - blue
-	//217,217,217 - gray
 
 	//Constructor 
 	public GUI_project(){
 
-		this.setTitle("Task Manager pre-alpha");
-		this.setSize(500,500);
+		this.setTitle("GUI_project");
+		this.setSize(509,396);
 		//menu generate method
-		//generateMenu();
-		//this.setJMenuBar(menuBar);
+		generateMenu();
+		this.setJMenuBar(menuBar);
 
 		//pane with null layout
 		JPanel contentPane = new JPanel(null);
-		contentPane.setPreferredSize(new Dimension(500,500));
-		contentPane.setBackground(new Color(50,109,255));
+		contentPane.setPreferredSize(new Dimension(509,396));
+		contentPane.setBackground(new Color(192,192,192));
 
 
 		button1 = new JButton();
-		button1.setBounds(8,89,50,50);
-		button1.setBackground(new Color(217,217,217));
+		button1.setBounds(10,94,90,35);
+		button1.setBackground(new Color(214,217,223));
 		button1.setForeground(new Color(0,0,0));
 		button1.setEnabled(true);
-		button1.setFont(new Font("Arial",0,12));
+		button1.setFont(new Font("sansserif",0,12));
 		button1.setText("OK");
 		button1.setVisible(true);
 
 		combobox1 = new JComboBox();
-		combobox1.setBounds(5,42,90,35);
+		combobox1.setBounds(8,44,90,35);
 		combobox1.setBackground(new Color(214,217,223));
 		combobox1.setForeground(new Color(0,0,0));
 		combobox1.setEnabled(true);
-		combobox1.setFont(new Font("Arial",0,12));
+		combobox1.setFont(new Font("sansserif",0,12));
 		combobox1.setVisible(true);
 
 		label1 = new JLabel();
 		label1.setBounds(5,10,140,35);
-		label1.setBackground(new Color(50,109,255));
+		label1.setBackground(new Color(51,0,255));
 		label1.setForeground(new Color(0,0,0));
 		label1.setEnabled(true);
-		label1.setFont(new Font("Arial",0,18));
-		label1.setText("Event Type");
+		label1.setFont(new Font("sansserif",0,12));
+		label1.setText("Choose your Task type");
 		label1.setVisible(true);
 
 		panel2 = new JPanel(null);
-		panel2.setBounds(0,0,500,500);
-		panel2.setBackground(new Color(50, 109, 255));
+		panel2.setBorder(BorderFactory.createEtchedBorder(1));
+		panel2.setBounds(2,0,502,391);
+		panel2.setBackground(new Color(214,217,223));
 		panel2.setForeground(new Color(0,0,0));
 		panel2.setEnabled(true);
-		panel2.setFont(new Font("Arial",0,12));
+		panel2.setFont(new Font("sansserif",0,12));
 		panel2.setVisible(true);
 
 		//adding components to contentPane panel
@@ -89,14 +88,36 @@ public class GUI_project extends JFrame {
 	}
 
 	//method for generate menu
-	//public void generateMenu(){
-		//menuBar = new JMenuBar();
-	//}
+	public void generateMenu(){
+		menuBar = new JMenuBar();
+
+		JMenu file = new JMenu("File");
+		JMenu tools = new JMenu("Tools");
+		JMenu help = new JMenu("Help");
+
+		JMenuItem open = new JMenuItem("Open   ");
+		JMenuItem save = new JMenuItem("Save   ");
+		JMenuItem exit = new JMenuItem("Exit   ");
+		JMenuItem preferences = new JMenuItem("Preferences   ");
+		JMenuItem about = new JMenuItem("About   ");
+
+
+		file.add(open);
+		file.add(save);
+		file.addSeparator();
+		file.add(exit);
+		tools.add(preferences);
+		help.add(about);
+
+		menuBar.add(file);
+		menuBar.add(tools);
+		menuBar.add(help);
+	}
 
 
 
 	 public static void main(String[] args){
-		//System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new GUI_project();
