6696e6223bf9d51479756bee8bcb5b7c8d8befc7
eqian <eqian7@students.d125.org>
2015-05-18 09:11:22 -0500

    added more resources

--- TasksMadeEasyJavaPrgm/Resources.java
+++ TasksMadeEasyJavaPrgm/Resources.java
@@ -1,7 +1,11 @@
 public class Resources{
 	//Will add more
-	private final String GREETINGMESSAGE = "Welcome to Tasker!";
-	private final String ADDNEWTASKMESSAGE = "Please type in the Name of the Task";
-	private final String QUITMESSAGE = "Goodbye, storing Tasks";
-	private final String SUCCESSMESSAGE = "Task created!";
+	public static final String GREETINGMESSAGE = "Welcome to Tasker!";
+	public static final String ADDNEWTASKMESSAGE = "Please type in the Name of the Task";
+	public static final String QUITMESSAGE = "Goodbye, storing Tasks";
+	public static final String SUCCESSMESSAGE = "Task created!";
+	public static final String TASKREMINDER = "You have a task today!";
+	public static final String EVENTREMINDER = "You have an event today!";
+	public static final String WEATHER = "The weather today is: ";
+	
 }
