bd1d92df3fa12d18267acbbc3c05e31da4834e75
hudohudo <havenhillhudson@gmail.com>
2015-05-20 09:12:52 -0500

    fixed your dumb mistakes

--- TasksMadeEasyJavaPrgm/Anniversary.java
+++ TasksMadeEasyJavaPrgm/Anniversary.java
@@ -1,7 +1,8 @@
+
 public class Anniversary extends Event
 {
-	private List<Gifts> gifts = new Arraylist<Gifts>;
-	private people;
+	private List<Gifts> gifts = new Arraylist<Gifts>();
+	private int people;
 	public Anniversary (String nameOfEvent, Date dateOfEvent, int m, int d, int y, int att)
 	{
 		super(nameOfEvent, dateOfEvent, m, d, y);
@@ -12,9 +13,9 @@ public class Anniversary extends Event
 	public void addGift (String name, double cost)
 	{
 		Gift thing = new Gift(name, cost);
-		gifts.add(thing)
+		gifts.add(thing);
 	}
-	public getDate()
+	public Date getDate()
 	{
 		super.getDateToNotify();
 	}
