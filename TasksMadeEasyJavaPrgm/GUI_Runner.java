191d4185fd6905c0b285a2951527117b30659b5e
eqian <eqian7@students.d125.org>
2015-05-21 09:49:58 -0500

    Added runner

---
+++ TasksMadeEasyJavaPrgm/GUI_Runner.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+/**
+ * Write a description of class GUI_Runner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GUI_Runner
+{
+    public static void main (String [] args)
+    {
+        JFrame runner = new JFrame (); 
+        runner.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+    }
+}
