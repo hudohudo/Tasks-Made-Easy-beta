517a8d179884edd3c3d7c8e9735a3a17148ea69c
hudohudo <havenhillhudson@gmail.com>
2015-05-26 09:53:50 -0500

    stuff

--- TasksMadeEasyJavaPrgm/GUI_Runner.java
+++ TasksMadeEasyJavaPrgm/GUI_Runner.java
@@ -4,8 +4,11 @@ public class GUI_Runner
     public static void main (String [] args)
     {
         JFrame runner = new JFrame ("TasksMadeEasy BETA FTW");
+        
         runner.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        runner.getContentPane().add(new MyPanel());
         runner.getContentPane().add(new GUI_project());
+        
         runner.pack();
         runner.setVisible(true);
     }
