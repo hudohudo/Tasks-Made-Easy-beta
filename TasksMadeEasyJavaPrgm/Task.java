e4ef5300f50dceb6db7b8837bf0ce4f72ac4f603
eqian <eqian7@students.d125.org>
2015-05-26 09:20:18 -0500

    edited constructor

--- TasksMadeEasyJavaPrgm/Task.java
+++ TasksMadeEasyJavaPrgm/Task.java
@@ -4,10 +4,12 @@ public class Task
     private String taskName;    
     private Date taskDate;
     private Date  remind;
+    private String taskDescription;
 
-    public Task(String nameOfTask, int m, int d, int y, int m1, int d1, int y1)
+    public Task(String nameOfTask, int m, int d, int y, int m1, int d1, int y1, String description)
     {
         taskName = nameOfTask;
+        taskDescription = description;
         taskDate = new Date(y, m, d);
         remind = new Date(y1, m1, d1);
     }
@@ -17,12 +19,15 @@ public class Task
         taskName = val;
     }
 
-
+    public String getName()
+    {
+        return taskName;
+    }
+    
     public void setDate(int m, int d, int y)
     {
         taskDate = new Date(y, m, d);
     }
-
 
 
     public String getTaskName()
